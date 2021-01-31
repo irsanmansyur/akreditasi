c46a0e5a439e5e189977095fb59bf499e9b7308d
tantangin <irsan00mansyur@gmail.com>
2021-01-31 07:28:11 -0800

    standar 2

--- application/views/member/akreditasi/index.php
+++ application/views/member/akreditasi/index.php
@@ -11,9 +11,9 @@
     <table class="table table-bordered table-striped DataTablePrint">
       <thead>
         <tr>
-           <th style="text-align: center; vertical-align: middle;" class="text-center" rowspan="3">No</th>
+          <th style="text-align: center; vertical-align: middle;" class="text-center" rowspan="3">No</th>
           <th style="text-align: center; vertical-align: middle;" rowspan="3">Status Akreditasi</th>
-      
+
         </tr>
         <tr>
           <th colspan="3">Akademik</th>
@@ -40,23 +40,23 @@
 
             <?php
             $total = 0;
-            foreach ($subjenjang as $key2 => $value2) : ?>
+            foreach ($listJenjang as $key2 => $value2) : ?>
               <?php
               $this->db->where('id_akreditasi', $value->akreditasi_id);
-              $this->db->where('id_subjenjang', $value2->subjenjang_id);
-              $id_sub_jenjang = $value2->subjenjang_id;
+              $this->db->where('id_subjenjang', $value2->jenjang_id);
+              $id_sub_jenjang = $value2->jenjang_id;
 
 
               $jml = $this->db->count_all_results('tbl_prodi');
               $total = $total + $jml;
               //$this->db->where('akreditasi_id',$value->akreditasi_id)->where('id_subjenjang',$value2->subjenjang_id)->get('tbl_prodi')->count_all_results(); 
               ?>
-              <td class=""><a href="<?php echo base_url('admin/prodi/breakdown_prodi/'.$id_sub_jenjang.'/'.$value->akreditasi_id) ?>"><?= $jml; ?></a></td>
+              <td class=""><a href="<?php echo base_url('admin/prodi/breakdown_prodi/' . $id_sub_jenjang . '/' . $value->akreditasi_id) ?>"><?= $jml; ?></a></td>
             <?php endforeach; ?>
 
-              <td class=""><a href="<?php echo base_url('admin/prodi/breakdown_prodi/'.$id_sub_jenjang.'/'.$value->akreditasi_id) ?>"><?= $total; ?></a></td>
-            </tr>
-           <?php endforeach; ?>
+            <td class=""><a href="<?php echo base_url('admin/prodi/breakdown_prodi/' . $id_sub_jenjang . '/' . $value->akreditasi_id) ?>"><?= $total; ?></a></td>
+          </tr>
+        <?php endforeach; ?>
       </tbody>
     </table>
   </div>
--- application/views/member/prodi/listing.php
+++ application/views/member/prodi/listing.php
@@ -1,48 +1,54 @@
-</head> 
-<body>
-    <div class="container">
-        <h1 style="font-size:20pt"></h1>
+</head>
 
-        <h3></h3>
-        <br />
-        <a href="<?= base_url('admin/akreditasi') ?>" class="btn btn-danger  btn-xl"></i>Kembali</a><br><br>
-    <table class="table table-bordered table-striped  DataTablePrint">
-        
-        
-        <table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
-	<thead>
-		<tr>
-			<th>No</th>
-			<th>Strata</th>
-			<th>Fakultas</th>
-			<th>Prodi</th>
-			<th>No SK</th>
-			<th>Tahun SK</th>
-			<th>Daluarsa</th>
-			<th>Sertifikat</th>
-			
-		</tr>
-	</thead>
-	<tbody>
-		<?php $i=1; foreach ($listing as $listing) { ?>
-		<tr>
-			<td><?php echo $i ?></td>
-			<td><?php echo $listing->subjenjang_nama ?></td>
-			<td><?php echo $listing->fakultas_nama ?></td>
-			<td><?php echo $listing->prodi_nama ?></td>
-			<td><?php echo $listing->no_sk ?></td>
-			<td><?php echo $listing->tahun_sk ?></td>
-			<td><?php echo $listing->daluarsa ?></td>
-			<td> <a href ="<?php echo base_url()?>uploads/sertifikat/<?php echo $listing->sertifikat ?>">Sertifikat </a></td> 
-			
-              
-            </td>
-		</tr>
-	<?php $i++; } ?>
-	</tbody>
-</table>
-<?php echo $this->uri->segment(5);?>
-<div class="panel panel-default">
-  <div class="panel-heading clearfix">
-   
- 
+<body>
+	<div class="container">
+		<h1 style="font-size:20pt"></h1>
+
+		<h3></h3>
+		<br />
+		<a href="<?= base_url('admin/akreditasi') ?>" class="btn btn-danger  btn-xl"></i>Kembali</a><br><br>
+		<table class="table table-bordered table-striped  DataTablePrint">
+
+
+			<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Strata</th>
+						<th>Fakultas</th>
+						<th>Prodi</th>
+						<th>No SK</th>
+						<th>Tahun SK</th>
+						<th>Daluarsa</th>
+						<th>Sertifikat</th>
+
+					</tr>
+				</thead>
+				<tbody>
+					<?php $i = 1;
+					foreach ($listing as $listing) { ?>
+						<tr>
+							<td><?php echo $i ?></td>
+							<td><?php echo $listing->subjenjang_nama ?></td>
+							<td><?php echo $listing->fakultas_nama ?></td>
+							<td><?php echo $listing->prodi_nama ?></td>
+							<td><?php echo $listing->no_sk ?></td>
+							<td><?php echo $listing->tahun_sk ?></td>
+							<td><?php echo $listing->daluarsa ?></td>
+							<td>
+								<?php if ($listing->sertifikat == "") : ?>
+									<a href="#tidakadasertifikat" class="badge badge-primary">Tidak tersedia </a>
+								<?php else : ?>
+									<a href="<?php echo base_url() ?>uploads/sertifikat/<?php echo $listing->sertifikat ?>">Sertifikat </a>
+								<?php endif; ?>
+							</td>
+
+							</td>
+						</tr>
+					<?php $i++;
+					} ?>
+				</tbody>
+			</table>
+			<?php echo $this->uri->segment(5); ?>
+			<div class="panel panel-default">
+				<div class="panel-heading clearfix">
